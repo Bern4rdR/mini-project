051f8b9e34ac163ac3e6b2253b05ecd3f5501ca1
bernardr <bernardr@kth.se>
2023-12-06 14:13:42 +0100

    add functions to initialize, send and recieve display

--- src/renderer.c
+++ src/renderer.c
@@ -6,16 +6,56 @@
 #include <pic32mx.h>
 
 
+// display helper functions
+#define DISPLAY_CHANGE_TO_COMMAND_MODE (PORTFCLR = 0x10)
+#define DISPLAY_CHANGE_TO_DATA_MODE (PORTFSET = 0x10)
+
+#define DISPLAY_ACTIVATE_RESET (PORTGCLR = 0x200)
+#define DISPLAY_DO_NOT_RESET (PORTGSET = 0x200)
+
+#define DISPLAY_ACTIVATE_VDD (PORTFCLR = 0x40)
+#define DISPLAY_ACTIVATE_VBAT (PORTFCLR = 0x20)
+
+#define DISPLAY_TURN_OFF_VDD (PORTFSET = 0x40)
+#define DISPLAY_TURN_OFF_VBAT (PORTFSET = 0x20)
+
+
+uint8_t spi_send_recv(uint8_t data) {
+	while(!(SPI2STAT & 0x08));
+	SPI2BUF = data;
+	while(!(SPI2STAT & 1));
+	return SPI2BUF;
+}
+
 void display_image(int x, unsigned int* data) {
+	int i, j;
     for(i = 0; i < 4; i++) {
-		PORTFCLR = 0X10;
+		DISPLAY_CHANGE_TO_COMMAND_MODE;
 		spi_send_recv(0x22);
 		spi_send_recv(i);
 		
 		spi_send_recv(0x0);
 		spi_send_recv(0x10);
+		DISPLAY_CHANGE_TO_DATA_MODE;
 		
-		PORTFSET = 0X10;
+		for(j = 0; j < 16; j++) {
+			// display works with inverse data (1 is off, 0 is on)
+			spi_send_recv(~data[i*16 + j]);
+		}
+	}
+}
+
+void display_update(void) {
+    int i, j, k;
+    int c;
+    for(i = 0; i < 4; i++) {
+		DISPLAY_CHANGE_TO_COMMAND_MODE;
+		spi_send_recv(0x22);
+		spi_send_recv(i);
+		
+		spi_send_recv(0x0);
+		spi_send_recv(0x10);
+		DISPLAY_CHANGE_TO_DATA_MODE;
 		
 		for(j = 0; j < 16; j++) {
 			c = textbuffer[i][j];
@@ -28,26 +68,32 @@ void display_image(int x, unsigned int* data) {
 	}
 }
 
-void display_update(void) {
-    int i, j, k;
-    int c;
-    for(i = 0; i < 4; i++) {
-		PORTFCLR = 0X10;
-		spi_send_recv(0x22);
-		spi_send_recv(i);
-		
-		spi_send_recv(0x0);
-		spi_send_recv(0x10);
-		
-		PORTFSET = 0X10;
-		
-		for(j = 0; j < 16; j++) {
-			c = textbuffer[i][j];
-			if(c & 0x80)
-				continue;
-			
-			for(k = 0; k < 8; k++)
-				spi_send_recv(font[c*8 + k]);
-		}
-	}
+void display_init(void) {
+        DISPLAY_CHANGE_TO_COMMAND_MODE;
+	quicksleep(10);
+	DISPLAY_ACTIVATE_VDD;
+	quicksleep(1000000);
+	
+	spi_send_recv(0xAE);
+	DISPLAY_ACTIVATE_RESET;
+	quicksleep(10);
+	DISPLAY_DO_NOT_RESET;
+	quicksleep(10);
+	
+	spi_send_recv(0x8D);
+	spi_send_recv(0x14);
+	
+	spi_send_recv(0xD9);
+	spi_send_recv(0xF1);
+	
+	DISPLAY_ACTIVATE_VBAT;
+	quicksleep(10000000);
+	
+	spi_send_recv(0xA1);
+	spi_send_recv(0xC8);
+	
+	spi_send_recv(0xDA);
+	spi_send_recv(0x20);
+	
+	spi_send_recv(0xAF);
 }
