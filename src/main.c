4be2263580968583fa4f77887ca53a5bc79415f5
bernardr <bernardr@kth.se>
2023-12-06 14:43:53 +0100

    moved function to main

--- src/main.c
+++ src/main.c
@@ -1,3 +1,11 @@
+#include <pic32mx.h>
+
+#include "render.h"
+#include "control.h"
+#include "logic.h"
+#include "screen.h"
+
+
 void main() {
     // setup/initializse the system and screen
     // start in main menu
