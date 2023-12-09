56453113c80e4b26df897a3dbf5b29e3e58d48b7
bernardr <bernardr@kth.se>
2023-12-09 19:17:24 +0100

    add call using analogRead

--- src/gyroControl.c
+++ src/gyroControl.c
@@ -28,28 +28,28 @@ void user_input(int* walking, float* playerDirection) {
     }
 
     // convert to a float between 0 and 1
-    float potentiometerFloat = (float)readADC() / 1023;
+    float potentiometerFloat = (float)analogRead(A0) / 1023;
     // convert to a float between 0 and 2pi
     *playerDirection = potentiometerFloat * 2 * PI;
 }
 
 // Initialize the ADC module to read the potentiometer value
-void initADC() {
-    AD1PCFG = 0xFFFB; // Set PORTB<2> as analog input
-    AD1CON1 = 0x00E0; // Set auto-convert mode
-    AD1CSSL = 0; // No scanning required
-    AD1CON2 = 0; // Use MUXA, AVss and AVdd as Vref
-    AD1CON3 = 0x1F3F; // Set Tad and sample time
-    AD1CON1SET = 0x8000; // Enable the ADC module
-}
+// void initADC() {
+//     AD1PCFG = 0xFFFB; // Set PORTB<2> as analog input
+//     AD1CON1 = 0x00E0; // Set auto-convert mode
+//     AD1CSSL = 0; // No scanning required
+//     AD1CON2 = 0; // Use MUXA, AVss and AVdd as Vref
+//     AD1CON3 = 0x1F3F; // Set Tad and sample time
+//     AD1CON1SET = 0x8000; // Enable the ADC module
+// }
 
 // Read the ADC value from channel 2 (AN2)
-int readADC() {
-  AD1CHSbits.CH0SA = 2; // Select AN2 as input
-  AD1CON1bits.SAMP = 1; // Start sampling
-  while (!AD1CON1bits.DONE); // Wait for conversion to finish
-  return ADC1BUF0; // Return the ADC value
-}
+// int readADC() {
+//   AD1CHSbits.CH0SA = 2; // Select AN2 as input
+//   AD1CON1bits.SAMP = 1; // Start sampling
+//   while (!AD1CON1bits.DONE); // Wait for conversion to finish
+//   return ADC1BUF0; // Return the ADC value
+// }
 
 int getbtns(void) {
     // returns the value of the buttons
